a074a4b46093d4a0cb6d2a4a4e243c0ebfdb5b1d
Naif <50641595+nnnaif@users.noreply.github.com>
2020-11-08 15:03:37 +0300

    feat: integrate geonames API

--- src/client/js/app.js
+++ src/client/js/app.js
@@ -11,4 +11,22 @@ document.getElementById('tripForm').addEventListener('submit', (event) => {
 /* Function to handle form input coming from event listener */
 const tripFormHandler = (formData) => {
   console.log(formData);
+  getCoords(formData.destination).then((res) => {
+    console.log(res);
+  });
+};
+
+const getCoords = (cityName) => {
+  const apiUser = 'fend_naif';
+  return fetch(
+    'http://api.geonames.org/searchJSON?q=' + cityName + '&username=' + apiUser,
+  )
+    .then((res) => res.json())
+    .then((res) => {
+      return {
+        latitude: res.geonames[0].lat,
+        longitude: res.geonames[0].lng,
+        country: res.geonames[0].countryName,
+      };
+    });
 };
